03c45dc30d2eee5ed38d639c2699774c403fc698
cc25346 <cc25346@g.unicamp.br>
2026-03-09 18:03:32 -0300

    Rename Reagente to ParteEq and update structure
    Refactor classes and add methods for chemical equations.

--- Ideias/Teste01.cs
+++ Ideias/Teste01.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
-namespace Reagentes
+namespace Balanceador
 {
   class Elemento{
     private int z;//Numero Atomico se representa com Z
@@ -71,11 +72,11 @@ namespace Reagentes
         } 
     }
 
-    class Reagente
+    class ParteEq
     {
         public List<Molecula> Moleculas {get; set;}
 
-        public Reagente()
+        public ParteEq()
         {
             Moleculas = new List<Molecula>();
         } 
@@ -100,11 +101,49 @@ namespace Reagentes
 
     class EquacaoQuimica
     {
-        //vai pegar um input de uma equação e transformala nas classes
-        //nao seria mais facil so fazer direto do input?
-        //armazenar todos os atomos nesse formato?
-        //armazenar as moleculas uteis e ir adicionando com o tempo??
-        //ter as reaçoes ja montadas??
+        public List<ParteEq> Reagente;
+        public List<ParteEq> Produto;
+
+        public EquacaoQuimica(){
+            Reagente = new List<ParteEq>();
+            Produto = new List<ParteEq>();
+        }
+
+        public List<int> IndentificaAtomosUnicos(ParteEq parte)
+        {
+            List<int> atomosUnicos = new List<int>(); 
+            foreach (var item in parte.Moleculas)
+            {
+                foreach(var itemP in item.Elementos)
+                {
+                 if(!atomosUnicos.Contains(itemP.Z))
+                    {
+                        atomosUnicos.Add(itemP.Z);
+                    } 
+                }
+            }
+
+            return atomosUnicos;
+        }
+
+        
+        /*Hoje 9 de março em campinas-COTUCA-LabLapa as 17:00 Rafael Schmal
+        fiz o esqueleto de um programa que balanceia equaçoes
+        tentei fazer com C# em POO com as classes Elemento, Molecula, ParteEq e EquaçãoQuimica
+        a ideia é ter os metodos que serao acessados pela interação do usuario
+        em que os Elementos ja vao ser definidos previamente com a tabela periodica ou banco de dados;
+        e as Moleculas tambem; o que possibilitaria o facil acesso as funcionalidades;
+
+        18:30 - ADD Vitor Ohland por Watszap
+        fiz todo o esqueleto agora passei para os metodos vou fazer o metodo ContarAtomo para Elemento,molecula e ParteQe;
+        fazer o metodo indentificarAtomosUnicos para Equaçãoquimica porque tive a idaia de Balanciar por matriz e resolver a matriz por gauss
+        */ 
+
+        //vai pegar um input de uma equação e transformala nas classes -X
+        //nao seria mais facil so fazer direto do input?-X
+        //armazenar todos os atomos nesse formato?-X
+        //armazenar as moleculas uteis e ir adicionando com o tempo??-X
+        //ter as reaçoes ja montadas??-X
         //Gerar por matriz, classificar todos os elementos unicos e montar sistema de equação 
         /*
             gera lista de elementos únicos
