976524567df960ce021be1235cda5d9b6ab33056
cc25572 <cc25572@g.unicamp.br>
2026-05-11 17:39:22 -0300

    Enhance UIReacao with additional chemical substances
    Updated the chemical reaction options and added more substances to the dictionary.

--- unity/Scripts/UIReacao.cs
+++ unity/Scripts/UIReacao.cs
@@ -8,13 +8,101 @@ public class UIReacao : MonoBehaviour
     public TMP_Dropdown dropPart1;
     public TMP_Dropdown dropPart2;
 
-    private Dictionary<string, Dictionary<int, int>> banco = new Dictionary<string, Dictionary<int, int>>()
+    private readonly Dictionary<string, Dictionary<int, int>> banco = new()
     {
-        { "H2", new Dictionary<int, int> { {1, 2} } },
-        { "O2", new Dictionary<int, int> { {8, 2} } },
-        { "H2O", new Dictionary<int, int> { {1, 2}, {8, 1} } },
-        { "CO2", new Dictionary<int, int> { {6, 1}, {8, 2} } },
-        { "C", new Dictionary<int, int> { {6, 1} } }
+        // ── Substâncias simples ────────────────────────────────────
+        { "H2",   new Dictionary<int, int> { { Atomos.H,  2 } } },
+        { "O2",   new Dictionary<int, int> { { Atomos.O,  2 } } },
+        { "N2",   new Dictionary<int, int> { { Atomos.N,  2 } } },
+        { "F2",   new Dictionary<int, int> { { Atomos.F,  2 } } },
+        { "Cl2",  new Dictionary<int, int> { { Atomos.Cl, 2 } } },
+        { "Br2",  new Dictionary<int, int> { { Atomos.Br, 2 } } },
+        { "I2",   new Dictionary<int, int> { { Atomos.I,  2 } } },
+        { "C",    new Dictionary<int, int> { { Atomos.C,  1 } } },
+        { "S",    new Dictionary<int, int> { { Atomos.S,  1 } } },
+        { "P",    new Dictionary<int, int> { { Atomos.P,  1 } } },
+        { "Na",   new Dictionary<int, int> { { Atomos.Na, 1 } } },
+        { "K",    new Dictionary<int, int> { { Atomos.K,  1 } } },
+        { "Ca",   new Dictionary<int, int> { { Atomos.Ca, 1 } } },
+        { "Mg",   new Dictionary<int, int> { { Atomos.Mg, 1 } } },
+        { "Al",   new Dictionary<int, int> { { Atomos.Al, 1 } } },
+        { "Fe",   new Dictionary<int, int> { { Atomos.Fe, 1 } } },
+        { "Cu",   new Dictionary<int, int> { { Atomos.Cu, 1 } } },
+        { "Zn",   new Dictionary<int, int> { { Atomos.Zn, 1 } } },
+
+        // ── Óxidos ────────────────────────────────────────────────
+        { "H2O",  new Dictionary<int, int> { { Atomos.H,  2 }, { Atomos.O, 1 } } },
+        { "CO2",  new Dictionary<int, int> { { Atomos.C,  1 }, { Atomos.O, 2 } } },
+        { "CO",   new Dictionary<int, int> { { Atomos.C,  1 }, { Atomos.O, 1 } } },
+        { "SO2",  new Dictionary<int, int> { { Atomos.S,  1 }, { Atomos.O, 2 } } },
+        { "SO3",  new Dictionary<int, int> { { Atomos.S,  1 }, { Atomos.O, 3 } } },
+        { "NO",   new Dictionary<int, int> { { Atomos.N,  1 }, { Atomos.O, 1 } } },
+        { "NO2",  new Dictionary<int, int> { { Atomos.N,  1 }, { Atomos.O, 2 } } },
+        { "N2O",  new Dictionary<int, int> { { Atomos.N,  2 }, { Atomos.O, 1 } } },
+        { "Fe2O3",new Dictionary<int, int> { { Atomos.Fe, 2 }, { Atomos.O, 3 } } },
+        { "Fe3O4",new Dictionary<int, int> { { Atomos.Fe, 3 }, { Atomos.O, 4 } } },
+        { "CaO",  new Dictionary<int, int> { { Atomos.Ca, 1 }, { Atomos.O, 1 } } },
+        { "MgO",  new Dictionary<int, int> { { Atomos.Mg, 1 }, { Atomos.O, 1 } } },
+        { "Al2O3",new Dictionary<int, int> { { Atomos.Al, 2 }, { Atomos.O, 3 } } },
+        { "Na2O", new Dictionary<int, int> { { Atomos.Na, 2 }, { Atomos.O, 1 } } },
+        { "K2O",  new Dictionary<int, int> { { Atomos.K,  2 }, { Atomos.O, 1 } } },
+        { "ZnO",  new Dictionary<int, int> { { Atomos.Zn, 1 }, { Atomos.O, 1 } } },
+        { "CuO",  new Dictionary<int, int> { { Atomos.Cu, 1 }, { Atomos.O, 1 } } },
+        { "P2O5", new Dictionary<int, int> { { Atomos.P,  2 }, { Atomos.O, 5 } } },
+
+        // ── Ácidos ────────────────────────────────────────────────
+        { "HCl",  new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.Cl, 1 } } },
+        { "HF",   new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.F,  1 } } },
+        { "HBr",  new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.Br, 1 } } },
+        { "HI",   new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.I,  1 } } },
+        { "H2S",  new Dictionary<int, int> { { Atomos.H,  2 }, { Atomos.S,  1 } } },
+        { "H2SO4",new Dictionary<int, int> { { Atomos.H,  2 }, { Atomos.S,  1 }, { Atomos.O, 4 } } },
+        { "H2SO3",new Dictionary<int, int> { { Atomos.H,  2 }, { Atomos.S,  1 }, { Atomos.O, 3 } } },
+        { "HNO3", new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.N,  1 }, { Atomos.O, 3 } } },
+        { "HNO2", new Dictionary<int, int> { { Atomos.H,  1 }, { Atomos.N,  1 }, { Atomos.O, 2 } } },
+        { "H3PO4",new Dictionary<int, int> { { Atomos.H,  3 }, { Atomos.P,  1 }, { Atomos.O, 4 } } },
+        { "H2CO3",new Dictionary<int, int> { { Atomos.H,  2 }, { Atomos.C,  1 }, { Atomos.O, 3 } } },
+
+        // ── Bases ─────────────────────────────────────────────────
+        { "NaOH", new Dictionary<int, int> { { Atomos.Na, 1 }, { Atomos.O,  1 }, { Atomos.H, 1 } } },
+        { "KOH",  new Dictionary<int, int> { { Atomos.K,  1 }, { Atomos.O,  1 }, { Atomos.H, 1 } } },
+        { "Ca(OH)2", new Dictionary<int, int> { { Atomos.Ca, 1 }, { Atomos.O, 2 }, { Atomos.H, 2 } } },
+        { "Mg(OH)2", new Dictionary<int, int> { { Atomos.Mg, 1 }, { Atomos.O, 2 }, { Atomos.H, 2 } } },
+        { "Al(OH)3", new Dictionary<int, int> { { Atomos.Al, 1 }, { Atomos.O, 3 }, { Atomos.H, 3 } } },
+        { "NH3",  new Dictionary<int, int> { { Atomos.N,  1 }, { Atomos.H,  3 } } },
+
+        // ── Sais ──────────────────────────────────────────────────
+        { "NaCl", new Dictionary<int, int> { { Atomos.Na, 1 }, { Atomos.Cl, 1 } } },
+        { "KCl",  new Dictionary<int, int> { { Atomos.K,  1 }, { Atomos.Cl, 1 } } },
+        { "CaCl2",new Dictionary<int, int> { { Atomos.Ca, 1 }, { Atomos.Cl, 2 } } },
+        { "MgCl2",new Dictionary<int, int> { { Atomos.Mg, 1 }, { Atomos.Cl, 2 } } },
+        { "AlCl3",new Dictionary<int, int> { { Atomos.Al, 1 }, { Atomos.Cl, 3 } } },
+        { "FeCl2",new Dictionary<int, int> { { Atomos.Fe, 1 }, { Atomos.Cl, 2 } } },
+        { "FeCl3",new Dictionary<int, int> { { Atomos.Fe, 1 }, { Atomos.Cl, 3 } } },
+        { "CuCl2",new Dictionary<int, int> { { Atomos.Cu, 1 }, { Atomos.Cl, 2 } } },
+        { "ZnCl2",new Dictionary<int, int> { { Atomos.Zn, 1 }, { Atomos.Cl, 2 } } },
+        { "Na2SO4",new Dictionary<int, int> { { Atomos.Na, 2 }, { Atomos.S, 1 }, { Atomos.O, 4 } } },
+        { "CaSO4", new Dictionary<int, int> { { Atomos.Ca, 1 }, { Atomos.S, 1 }, { Atomos.O, 4 } } },
+        { "MgSO4", new Dictionary<int, int> { { Atomos.Mg, 1 }, { Atomos.S, 1 }, { Atomos.O, 4 } } },
+        { "Na2CO3",new Dictionary<int, int> { { Atomos.Na, 2 }, { Atomos.C, 1 }, { Atomos.O, 3 } } },
+        { "CaCO3", new Dictionary<int, int> { { Atomos.Ca, 1 }, { Atomos.C, 1 }, { Atomos.O, 3 } } },
+        { "NaHCO3",new Dictionary<int, int> { { Atomos.Na, 1 }, { Atomos.H, 1 }, { Atomos.C, 1 }, { Atomos.O, 3 } } },
+        { "NH4Cl", new Dictionary<int, int> { { Atomos.N,  1 }, { Atomos.H, 4 }, { Atomos.Cl, 1 } } },
+        { "KNO3",  new Dictionary<int, int> { { Atomos.K,  1 }, { Atomos.N, 1 }, { Atomos.O, 3 } } },
+        { "NaNO3", new Dictionary<int, int> { { Atomos.Na, 1 }, { Atomos.N, 1 }, { Atomos.O, 3 } } },
+
+        // ── Compostos orgânicos simples ────────────────────────────
+        { "CH4",  new Dictionary<int, int> { { Atomos.C,  1 }, { Atomos.H,  4 } } },
+        { "C2H6", new Dictionary<int, int> { { Atomos.C,  2 }, { Atomos.H,  6 } } },
+        { "C3H8", new Dictionary<int, int> { { Atomos.C,  3 }, { Atomos.H,  8 } } },
+        { "C4H10",new Dictionary<int, int> { { Atomos.C,  4 }, { Atomos.H, 10 } } },
+        { "C2H4", new Dictionary<int, int> { { Atomos.C,  2 }, { Atomos.H,  4 } } },
+        { "C2H2", new Dictionary<int, int> { { Atomos.C,  2 }, { Atomos.H,  2 } } },
+        { "C6H6", new Dictionary<int, int> { { Atomos.C,  6 }, { Atomos.H,  6 } } },
+        { "CH3OH",new Dictionary<int, int> { { Atomos.C,  1 }, { Atomos.H,  4 }, { Atomos.O, 1 } } },
+        { "C2H5OH",new Dictionary<int,int> { { Atomos.C,  2 }, { Atomos.H,  6 }, { Atomos.O, 1 } } },
+        { "C6H12O6",new Dictionary<int,int>{ { Atomos.C,  6 }, { Atomos.H, 12 }, { Atomos.O, 6 } } },
+        { "C12H22O11",new Dictionary<int,int>{{ Atomos.C,12 }, { Atomos.H, 22 }, { Atomos.O,11 } } },
     };
 
     private List<string> opcoes = new List<string>()
