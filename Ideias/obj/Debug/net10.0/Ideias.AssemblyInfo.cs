e047aa12cca551f88a29c1fba47d7a63bc94e09f
Rafael <cc25346@g.unicamp.br>
2026-08-27 15:23:29 -0300

    Add UIReacao class alternatlve (testing whitch is better)

--- Ideias/obj/Debug/net10.0/Ideias.AssemblyInfo.cs
+++ Ideias/obj/Debug/net10.0/Ideias.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ideias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1a234eb24d459c02870e3e9260acfcec686ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43892b001044ff91f5e2fcdbb2d4123a1d024b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ideias")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ideias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
